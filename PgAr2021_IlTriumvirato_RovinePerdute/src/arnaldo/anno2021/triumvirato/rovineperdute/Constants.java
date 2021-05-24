74348fd312d004f44645f0c27de0a4aed9c814f7
DavideMottini8192 <77574818+DavideMottini8192@users.noreply.github.com>
2021-05-24 21:29:32 +0100

    Re-upload, commit input1
    Vedere il prossimo commit in cui i file verranno effettivamente aggiunti.

--- PgAr2021_IlTriumvirato_RovinePerdute/src/arnaldo/anno2021/triumvirato/rovineperdute/Constants.java
+++
@@ -1,9 +0,0 @@
-package arnaldo.anno2021.triumvirato.rovineperdute;
-
-public class Constants {
-
-	public static final String MAP_PATHNAME = null;
-	public static final String TEAM1_NAME = null;
-	public static final String TEAM2_NAME = null;
-
-}
